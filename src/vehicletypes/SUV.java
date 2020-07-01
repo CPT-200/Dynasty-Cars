b83b91a01302356474fed35cde1755934cdac153
rnjmur <66527652+SCCC-rnjmur@users.noreply.github.com>
2020-06-30 21:28:57 -0500

    Create SUV.java

---
+++ src/vehicletypes/SUV.java
@@ -0,0 +1,8 @@
+package vehicletypes;
+
+public class SUV extends Car {
+	public SUV() {} // Default Constructor
+	public SUV(String brand, String model, String color, String fuelType, int mpg) { 
+		super(brand, model, color, fuelType, mpg);
+	}
+}
