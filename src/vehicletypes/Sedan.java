a1d4167f2a9584bc02fbaf73c587777a6a458eb2
rnjmur <66527652+SCCC-rnjmur@users.noreply.github.com>
2020-06-30 21:28:59 -0500

    Create Sedan.java

---
+++ src/vehicletypes/Sedan.java
@@ -0,0 +1,8 @@
+package vehicletypes;
+
+public class Sedan extends Car {
+	public Sedan() {} //Default Constructor
+	public Sedan(String brand, String model, String color, String fuelType, int mpg) { 
+		super(brand, model, color, fuelType, mpg);
+	}
+}
