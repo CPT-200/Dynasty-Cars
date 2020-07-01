d070d498b5e84f7525d43292757077df36904b3d
rnjmur <66527652+SCCC-rnjmur@users.noreply.github.com>
2020-06-30 21:29:02 -0500

    Create Coupe.java

---
+++ src/vehicletypes/Coupe.java
@@ -0,0 +1,8 @@
+package vehicletypes;
+
+public class Coupe extends Car {
+	public Coupe() {} //Default Constructor
+	public Coupe(String brand, String model, String color, String fuelType, int mpg) { 
+		super(brand, model, color, fuelType, mpg);
+	}
+}
