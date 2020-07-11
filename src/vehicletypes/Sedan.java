02c7eb54e1a8997d9529cef84ddc27139e7522b7
rnjmur <66527652+SCCC-rnjmur@users.noreply.github.com>
2020-07-10 22:15:25 -0500

    Update Sedan.java

--- src/vehicletypes/Sedan.java
+++ src/vehicletypes/Sedan.java
@@ -2,7 +2,11 @@ package vehicletypes;
 
 public class Sedan extends Car {
 	public Sedan() {} //Default Constructor
-	public Sedan(String brand, String model, String color, String fuelType, int mpg) { 
-		super(brand, model, color, fuelType, mpg);
+	public Sedan(String make, String model, int year, String color, String engine, String transmission) { 
+		super(make, model, year, color, engine, transmission);
+	}
+	
+	public String toString() {
+		return "Sedan " + super.toString();
 	}
 }
